3a1aa5680bcbb99f55b108b708a9fe92aaaa7c09
suryasnata <suryasnatamohanty8594@gmailcom>
2024-08-28 23:43:41 +0530

    task-1 added

---
+++ class-1/index.js
@@ -0,0 +1,10 @@
+console.log("Welcome to the Backend World");
+
+// REPL :
+// JS Expression :
+// Use Variables :
+// Multiline Code :
+// Use (_) to get the last result
+// Editor :
+// Use Ctrl + Enter to execute the code and get the result in the console.
+// Use Ctrl + Shift + Enter to execute the code and get the result in the editor.
---
+++ class-2/index.js
@@ -0,0 +1,30 @@
+// Import module
+const fs = require("fs");
+
+// console.log(fs);
+
+// Create a file :
+// fs.writeFileSync("notes.txt", "I am learning node.js");
+// fs.writeFileSync(
+//   "notes.txt",
+//   "I am learning node.js and this is a file system.\n"
+// );
+// it will over-write
+
+// append file:
+// fs.appendFileSync("notes.txt", "I am learning Backend.\n");
+// It will append the string to the end of the file.
+// fs.appendFileSync("notes.txt", "I am Suryasnata Mohanty.");
+
+//Read File :
+// const readData = fs.readFileSync("./notes.txt");
+// console.log(readData.toString());
+
+//Rename File:
+// fs.renameSync("notes.txt", "files.txt");
+
+// Create Folder :
+// fs.mkdirSync("nodeFolder");
+
+// Delete Folder :
+// fs.rmdirSync("nodeFolder");
